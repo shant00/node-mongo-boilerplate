45591afdb6dcce792776234165cb3efb04198021
shant00 <sshanto.cse@gmail.com>
2023-07-31 01:40:09 +0600

    basic structure with error handler commomthings

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import { Server } from 'http'
+import mongoose from 'mongoose'
+import app from './app'
+import config from './share/config/index'
+import { errorLogger, logger } from './share/logger'
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
+  process.exit(1)
+})
+let server: Server
+async function boostrap() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    logger.info(`Database is connected successfully 🛢   `)
+
+    app.listen(config.port, () => {
+      logger.info(`Application  listening on port ${config.port}`)
+    })
+  } catch (err) {
+    errorLogger.error('Failed to connect database', err)
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
+}
+
+boostrap()
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received')
+  if (server) {
+    server.close()
+  }
+})
